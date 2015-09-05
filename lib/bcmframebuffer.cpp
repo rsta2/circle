2192a30401c499181bb0ca3a87920d2eaf0e2367
Jose Luis Sanchez <jspeccy@gmail.com>
2015-09-05 13:37:11 +0200

    - Add support for framebuffers with virtual resolution & depths < 8 bits.

--- lib/bcmframebuffer.cpp
+++ lib/bcmframebuffer.cpp
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2015  R. Stange <rsta2@o2online.de>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -23,7 +23,7 @@
 #include <circle/bcm2835.h>
 #include <circle/util.h>
 
-CBcmFrameBuffer::CBcmFrameBuffer (unsigned nWidth, unsigned nHeight, unsigned nDepth)
+CBcmFrameBuffer::CBcmFrameBuffer (unsigned nWidth, unsigned nHeight, unsigned nDepth, unsigned nVirtualWidth, unsigned nVirtualHeight)
 :	m_MailBox (MAILBOX_CHANNEL_FB)
 {
 	if (   nWidth  == 0
@@ -44,7 +44,12 @@ CBcmFrameBuffer::CBcmFrameBuffer (unsigned nWidth, unsigned nHeight, unsigned nD
 		}
 	}
 
-	if (nDepth == 8)
+	if (nVirtualWidth == 0 || nVirtualHeight == 0) {
+		nVirtualWidth = nWidth;
+		nVirtualHeight = nHeight;
+	}
+
+	if (nDepth <= 8)
 	{
 		m_pInfo = (Bcm2835FrameBufferInfo *) new u8 [  sizeof (Bcm2835FrameBufferInfo)
 							     + PALETTE_ENTRIES * sizeof (u16)];
@@ -58,8 +63,8 @@ CBcmFrameBuffer::CBcmFrameBuffer (unsigned nWidth, unsigned nHeight, unsigned nD
 
 	m_pInfo->Width      = nWidth;
 	m_pInfo->Height     = nHeight;
-	m_pInfo->VirtWidth  = nWidth;
-	m_pInfo->VirtHeight = nHeight;
+	m_pInfo->VirtWidth  = nVirtualWidth;
+	m_pInfo->VirtHeight = nVirtualHeight;
 	m_pInfo->Pitch      = 0;
 	m_pInfo->Depth      = nDepth;
 	m_pInfo->OffsetX    = 0;
@@ -76,7 +81,7 @@ CBcmFrameBuffer::~CBcmFrameBuffer (void)
 
 void CBcmFrameBuffer::SetPalette (u8 nIndex, u16 nColor)
 {
-	if (m_pInfo->Depth == 8)
+	if (m_pInfo->Depth <= 8 && nIndex < 256)
 	{
 		m_pInfo->Palette[nIndex] = nColor;
 	}
@@ -88,17 +93,17 @@ boolean CBcmFrameBuffer::Initialize (void)
 	DataSyncBarrier ();
 	u32 nResult = m_MailBox.WriteRead (GPU_MEM_BASE + (u32) m_pInfo);
 	InvalidateDataCache ();
-	
+
 	if (nResult != 0)
 	{
 		return FALSE;
 	}
-	
+
 	if (m_pInfo->BufferPtr == 0)
 	{
 		return FALSE;
 	}
-	
+
 	return TRUE;
 }
 
@@ -110,6 +115,16 @@ unsigned CBcmFrameBuffer::GetWidth (void) const
 unsigned CBcmFrameBuffer::GetHeight (void) const
 {
 	return m_pInfo->Height;
+}
+
+unsigned CBcmFrameBuffer::GetVirtWidth (void) const
+{
+	return m_pInfo->VirtWidth;
+}
+
+unsigned CBcmFrameBuffer::GetVirtHeight (void) const
+{
+	return m_pInfo->VirtHeight;
 }
 
 u32 CBcmFrameBuffer::GetPitch (void) const
