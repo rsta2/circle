ce862cb3095d26064106782b3c3bc3ad929126e9
rsta2 <rsta2@o2online.de>
2017-01-02 16:59:42 +0100

    Set version number to 28

--- include/circle/version.h
+++ include/circle/version.h
@@ -2,7 +2,7 @@
 // version.h
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014-2016  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2017  R. Stange <rsta2@o2online.de>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 #define CIRCLE_NAME			"Circle"
 
 #define CIRCLE_MAJOR_VERSION		28
-#define CIRCLE_VERSION_STRING		"28alpha"
+#define CIRCLE_VERSION_STRING		"28"
 
 #define OS_NAME				CIRCLE_NAME
 #define OS_VERSION			CIRCLE_VERSION_STRING
