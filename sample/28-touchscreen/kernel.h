7ef08e8d381a4dabc939280984bb04fc4cdc13ff
rsta2 <rsta2@o2online.de>
2017-01-02 16:48:05 +0100

    Official Raspberry Pi touch screen supported
    class CTouchScreenDevice added
    
    sample/28-touchscreen/ added
    
    addon/ugui/ updated to use touch screen
    
    README.md updated for Circle 28

--- sample/28-touchscreen/kernel.h
+++ sample/28-touchscreen/kernel.h
@@ -3,7 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2016  R. Stange <rsta2@o2online.de>
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -24,20 +24,13 @@
 #include <circle/actled.h>
 #include <circle/koptions.h>
 #include <circle/devicenameservice.h>
-#include <circle/cputhrottle.h>
 #include <circle/screen.h>
 #include <circle/serial.h>
 #include <circle/exceptionhandler.h>
 #include <circle/interrupt.h>
 #include <circle/timer.h>
 #include <circle/logger.h>
-#include "scopeconfig.h"
-#include "recorder.h"
-#include <circle/usb/dwhcidevice.h>
-#include <circle/gpioclock.h>
-#include <circle/gpiopin.h>
 #include <circle/input/touchscreen.h>
-#include <ugui/uguicpp.h>
 #include <circle/types.h>
 
 enum TShutdownMode
@@ -58,25 +51,25 @@ public:
 	TShutdownMode Run (void);
 
 private:
+	static void TouchScreenEventHandler (TTouchScreenEvent Event,
+					     unsigned nID, unsigned nPosX, unsigned nPosY);
+	
+private:
 	// do not change this order
 	CMemorySystem		m_Memory;
 	CActLED			m_ActLED;
 	CKernelOptions		m_Options;
 	CDeviceNameService	m_DeviceNameService;
-	CCPUThrottle		m_CPUThrottle;
 	CScreenDevice		m_Screen;
 	CSerialDevice		m_Serial;
 	CExceptionHandler	m_ExceptionHandler;
 	CInterruptSystem	m_Interrupt;
 	CTimer			m_Timer;
 	CLogger			m_Logger;
-	CScopeConfig		m_Config;
-	CRecorder		m_Recorder;
-	CDWHCIDevice		m_DWHCI;
-	CGPIOClock		m_Clock0;
-	CGPIOPin		m_ClockPin;
+
 	CTouchScreenDevice	m_TouchScreen;
-	CUGUI			m_GUI;
+
+	static CKernel *s_pThis;
 };
 
 #endif
