9cc9d08e8759a8e07166e5e13ecd8550a97444bd
rsta2 <rsta2@o2online.de>
2017-08-20 12:28:44 +0200

    addon/sensor/ added
    Driver for HC-SR04 Ultrasonic distance measuring module added
    Driver for MPU-6050 with I2C interface added

---
+++ addon/sensor/sample/hcsr04/kernel.cpp
@@ -0,0 +1,102 @@
+//
+// kernel.cpp
+//
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2014-2017  R. Stange <rsta2@o2online.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#include "kernel.h"
+
+#define HC_SR04_TRIGGER_PIN	17
+#define HC_SR04_ECHO_PIN	18
+
+static const char FromKernel[] = "kernel";
+
+CKernel::CKernel (void)
+:	m_Screen (m_Options.GetWidth (), m_Options.GetHeight ()),
+	m_Timer (&m_Interrupt),
+	m_Logger (m_Options.GetLogLevel (), &m_Timer),
+	m_HCSR04 (HC_SR04_TRIGGER_PIN, HC_SR04_ECHO_PIN)
+{
+	m_ActLED.Blink (5);	// show we are alive
+}
+
+CKernel::~CKernel (void)
+{
+}
+
+boolean CKernel::Initialize (void)
+{
+	boolean bOK = TRUE;
+
+	if (bOK)
+	{
+		bOK = m_Screen.Initialize ();
+	}
+
+	if (bOK)
+	{
+		bOK = m_Serial.Initialize (115200);
+	}
+
+	if (bOK)
+	{
+		CDevice *pTarget = m_DeviceNameService.GetDevice (m_Options.GetLogDevice (), FALSE);
+		if (pTarget == 0)
+		{
+			pTarget = &m_Screen;
+		}
+
+		bOK = m_Logger.Initialize (pTarget);
+	}
+
+	if (bOK)
+	{
+		bOK = m_Interrupt.Initialize ();
+	}
+
+	if (bOK)
+	{
+		bOK = m_Timer.Initialize ();
+	}
+
+	if (bOK)
+	{
+		bOK = m_HCSR04.Initialize ();
+	}
+
+	return bOK;
+}
+
+TShutdownMode CKernel::Run (void)
+{
+	m_Logger.Write (FromKernel, LogNotice, "Compile time: " __DATE__ " " __TIME__);
+
+	while (1)
+	{
+		if (m_HCSR04.DoMeasurement ())
+		{
+			m_Logger.Write (FromKernel, LogNotice, "Distance is %u mm", m_HCSR04.GetDistance ());
+		}
+		else
+		{
+			m_Logger.Write (FromKernel, LogWarning, "Measurement failed");
+		}
+
+		m_Timer.MsDelay (500);
+	}
+
+	return ShutdownHalt;
+}
