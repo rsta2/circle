61f5a9a86e6fc27e06abedc05e8d612357867482
rsta2 <rsta2@o2online.de>
2018-01-24 12:37:06 +0100

    tools/flasher.py improved
    FIXED: Makefile of bootloader did not work with PREFIX
    defined in Config.mk.

--- tools/flasher.py
+++ tools/flasher.py
@@ -24,16 +24,21 @@ try:
     F = open(fileaddress,"r")
 except:
     print("ERROR: Cannot open file " + fileaddress + "\nCheck permissions!")
+    ser.close()
     exit()
 data = F.read()
+F.close()
 #data = struct.pack(hex, 0x7E, 0xFF, 0x03, 0x00, 0x01, 0x00, 0x02, 0x0A, 0x01, 0xC8,      0x04, 0xD0, 0x01, 0x02, 0x80, 0x00, 0x00, 0x00, 0x00, 0x8E, 0xE7, 0x7E)
 print("Flashing with baudrate of "+ str(flashbaud) + "...")
+sys.stdout.flush()
 try:
     ser.write(data)
+    print("Completed!\nRunning app...")
+    sys.stdout.flush()
+    sleep(1)
     ser.write("g")
 except SerialException:
     print("ERROR: Serial port disconnected.\nCheck connections!")
     ser.close()
     exit()
 ser.close()
-print("Completed!\nRunning app...")
