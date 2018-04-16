159f691fcb04c50f3ba41e2b83875a50876f584c
R. Stange <rsta2@o2online.de>
2018-04-16 16:49:41 +0200

    tools/flasher.py slightly improved

--- tools/flasher.py
+++ tools/flasher.py
@@ -1,12 +1,14 @@
 import serial
-from serial import SerialException
-import struct
 import sys
-from time import sleep
+import time
 
-fileaddress = ' '.join(sys.argv[1:2])
-flashport = ' '.join(sys.argv[2:3])
-flashbaud = int(' '.join(sys.argv[3:]))
+try:
+    fileaddress = ' '.join(sys.argv[1:2])
+    flashport = ' '.join(sys.argv[2:3])
+    flashbaud = int(' '.join(sys.argv[3:]))
+except Exception:
+    print("Usage: python flasher.py FILENAME SERIALPORT BAUDRATE")
+    exit()
 
 try:
     ser = serial.Serial(
@@ -16,29 +18,31 @@ try:
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS
     )
-except SerialException:
+except Exception:
     print("ERROR: Serial Port " + flashport + " busy or inexistent!")
     exit()
     
 try:
     F = open(fileaddress,"r")
-except:
-    print("ERROR: Cannot open file " + fileaddress + "\nCheck permissions!")
+    data = F.read()
+    F.close()
+except Exception:
+    print("ERROR: Cannot access file " + fileaddress + ". Check permissions!")
     ser.close()
     exit()
-data = F.read()
-F.close()
-#data = struct.pack(hex, 0x7E, 0xFF, 0x03, 0x00, 0x01, 0x00, 0x02, 0x0A, 0x01, 0xC8,      0x04, 0xD0, 0x01, 0x02, 0x80, 0x00, 0x00, 0x00, 0x00, 0x8E, 0xE7, 0x7E)
+
 print("Flashing with baudrate of "+ str(flashbaud) + "...")
 sys.stdout.flush()
+
 try:
     ser.write(data)
     print("Completed!\nRunning app...")
     sys.stdout.flush()
-    sleep(1)
+    time.sleep(1)
     ser.write("g")
-except SerialException:
-    print("ERROR: Serial port disconnected.\nCheck connections!")
+except Exception:
+    print("ERROR: Serial port disconnected. Check connections!")
     ser.close()
     exit()
+
 ser.close()
