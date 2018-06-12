cd84aaaae3028ab080d8ac5ca49678cb8bd0489f
R. Stange <rsta2@o2online.de>
2018-06-12 22:21:46 +0200

    Patches in addon/linux/ for compatibility with other headers
    Suggested by @jsanchezv

--- addon/linux/kernel.h
+++ addon/linux/kernel.h
@@ -11,16 +11,20 @@
 extern "C" {
 #endif
 
+#ifndef offsetof
 #ifdef __GNUC__
 	#define offsetof(type, member)	__builtin_offsetof (type, member)
 #else
 	#define offsetof(type, member)	((size_t)&((type *)0)->member)
 #endif
+#endif
 
 #define container_of(ptr, type, member) \
 	((type *) ((char *) (ptr) - offsetof (type, member)))
 
+#ifndef min
 #define min(a, b)	((a) < (b) ? (a) : (b))
+#endif
 
 #define sprintf		linuxemu_sprintf
 #define snprintf	linuxemu_snprintf
