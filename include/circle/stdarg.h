85ffc76341e6752bfb453fc4b27ee25485ecadb2
R. Stange <rsta2@o2online.de>
2019-12-09 13:18:05 +0100

    addon/littlevgl updated to v6.1.1
    Logging from LittlevGL implemented

--- include/circle/stdarg.h
+++ include/circle/stdarg.h
@@ -2,7 +2,7 @@
 // stdarg.h
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2019  R. Stange <rsta2@o2online.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,13 +17,19 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-#ifndef _stdarg_h
-#define _stdarg_h
+#ifndef _circle_stdarg_h
+#define _circle_stdarg_h
+
+// prevent warning, if <stdarg.h> from toolchain is included too
+#ifndef _STDARG_H
+#define _STDARG_H
 
 typedef __builtin_va_list va_list;
 
 #define va_start(arg, last)	__builtin_va_start (arg, last)
 #define va_end(arg)		__builtin_va_end (arg)
 #define va_arg(arg, type)	__builtin_va_arg (arg, type)
+
+#endif
 
 #endif
