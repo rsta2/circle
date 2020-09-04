82314d9bee8fc22c2edc0db9b0d8f96a036c18f5
Rene Stange <rsta2@o2online.de>
2020-09-04 21:26:21 +0200

    usb/usbmassdevice: Use CNumberPool for device number allocation

--- include/circle/usb/usbmassdevice.h
+++ include/circle/usb/usbmassdevice.h
@@ -2,7 +2,7 @@
 // usbmassdevice.h
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2014-2019  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2014-2020  R. Stange <rsta2@o2online.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@
 #include <circle/usb/usbfunction.h>
 #include <circle/usb/usbendpoint.h>
 #include <circle/fs/partitionmanager.h>
+#include <circle/numberpool.h>
 #include <circle/types.h>
 
 #define UMSD_BLOCK_SIZE		512
@@ -64,7 +65,7 @@ private:
 
 	CPartitionManager *m_pPartitionManager;
 
-	static unsigned s_nDeviceNumberMap;
+	static CNumberPool s_DeviceNumberPool;
 	unsigned m_nDeviceNumber;
 };
 
