f97b3bc35e1d1450b260c60dbbc694bf7f4c25d4
Rene Stange <rsta2@o2online.de>
2020-09-27 16:11:20 +0200

    Add sample/40-irqlatency

---
+++ sample/40-irqlatency/kernel.h
@@ -0,0 +1,86 @@
+//
+// kernel.h
+//
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2014-2020  R. Stange <rsta2@o2online.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#ifndef _kernel_h
+#define _kernel_h
+
+#include <circle/memory.h>
+#include <circle/actled.h>
+#include <circle/koptions.h>
+#include <circle/devicenameservice.h>
+#include <circle/screen.h>
+#include <circle/writebuffer.h>
+#include <circle/exceptionhandler.h>
+#include <circle/interrupt.h>
+#include <circle/timer.h>
+#include <circle/logger.h>
+#include <circle/usb/usbhcidevice.h>
+#include <circle/latencytester.h>
+#include <circle/types.h>
+
+#define USE_BUFFERED_SCREEN
+
+enum TShutdownMode
+{
+	ShutdownNone,
+	ShutdownHalt,
+	ShutdownReboot
+};
+
+class CKernel
+{
+public:
+	CKernel (void);
+	~CKernel (void);
+
+	boolean Initialize (void);
+
+	TShutdownMode Run (void);
+	
+private:
+	static void TimerHandler (TKernelTimerHandle hTimer, void *pParam, void *pContext);
+
+#ifdef USE_BUFFERED_SCREEN
+	static void PanicHandler (void);
+#endif
+
+private:
+	// do not change this order
+	CMemorySystem		m_Memory;
+	CActLED			m_ActLED;
+	CKernelOptions		m_Options;
+	CDeviceNameService	m_DeviceNameService;
+#ifdef USE_BUFFERED_SCREEN
+	CScreenDevice		m_ScreenUnbuffered;
+	CWriteBufferDevice	m_Screen;
+#else
+	CScreenDevice		m_Screen;
+#endif
+	CExceptionHandler	m_ExceptionHandler;
+	CInterruptSystem	m_Interrupt;
+	CTimer			m_Timer;
+	CLogger			m_Logger;
+	CUSBHCIDevice		m_USBHCI;
+
+	CLatencyTester		m_Latency;
+
+	static CKernel *s_pThis;
+};
+
+#endif
