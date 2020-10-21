fb4805a19b984808e827d6780deb60c2b6f99925
Hinko Kocevar <hinkocevar@gmail.com>
2020-10-21 20:55:42 +0200

    leaving my mark

--- lib/usb/usbmouse.cpp
+++ lib/usb/usbmouse.cpp
@@ -3,6 +3,7 @@
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
 // Copyright (C) 2014-2018  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2020  H. Kocevar <hinxx@protonmail.com>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
