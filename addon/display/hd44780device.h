ae4e38004763dadbe7b1a1c7712a3841ece93cbb
Kevin <diyelectromusic@gmail.com>
2022-05-13 13:41:29 +0100

    Extracted out character based display handling into its own class to allow addition of first draft of some simple ssd1306 based display support.

--- addon/display/hd44780device.h
+++ addon/display/hd44780device.h
@@ -25,38 +25,12 @@
 #include <circle/spinlock.h>
 #include <circle/types.h>
 #include <circle/i2cmaster.h>
-
-#define HD44780_MAX_COLUMNS	40
-#define HD44780_MAX_ROWS	4
+#include "chardevice.h"
 
 #define HD44780_CMD  0
 #define HD44780_DATA 1
 
-// ESCAPE SEQUENCES
-//
-// \E[B		Cursor down one line
-// \E[H		Cursor home
-// \E[A		Cursor up one line
-// \E[%d;%dH	Cursor move to row %1 and column %2 (starting at 1)
-// ^H		Cursor left one character
-// \E[D		Cursor left one character
-// \E[C		Cursor right one character
-// ^M		Carriage return
-// \E[J		Clear to end of screen
-// \E[K		Clear to end of line
-// \E[%dX	Erase %1 characters starting at cursor
-// ^J		Carriage return/linefeed
-// ^I		Move to next hardware tab
-// \E[?25h	Normal cursor visible
-// \E[?25l	Cursor invisible
-// \Ed+		Start autopage mode
-// \Ed*		End autopage mode
-//
-// ^X = Control character
-// \E = Escape (\x1b)
-// %d = Numerical parameter (ASCII)
-
-class CHD44780Device : public CDevice	/// LCD dot-matrix display driver (using HD44780 controller)
+class CHD44780Device : public CCharDevice	/// LCD dot-matrix display driver (using HD44780 controller)
 {
 public:
 	/// \param nColumns Display size in number of columns (max. 40)
@@ -88,18 +62,6 @@ public:
 	/// \return Operation successful?
 	boolean Initialize (void);
 
-	/// \return Display size in number of columns
-	unsigned GetColumns (void) const;
-	/// \return Display size in number of rows
-	unsigned GetRows (void) const;
-
-	/// \brief Write characters to the display
-	/// \note Supports several escape sequences (see above).
-	/// \param pBuffer Pointer to the characters to be written
-	/// \param nCount  Number of characters to be written
-	/// \return Number of written characters
-	int Write (const void *pBuffer, size_t nCount);
-
 	/// \brief Define the 5x7 font for the definable characters
 	/// \param chChar   Character code (0x80..0x87)
 	/// \param FontData Font bit map for character pixel line 0-7 (only bits 4-0 are used)
@@ -107,28 +69,11 @@ public:
 	void DefineCharFont (char chChar, const u8 FontData[8]);
 
 private:
-	void Write (char chChar);
-
-	void CarriageReturn (void);
-	void ClearDisplayEnd (void);
-	void ClearLineEnd (void);
-	void CursorDown (void);
-	void CursorHome (void);
-	void CursorLeft (void);
-	void CursorMove (unsigned nRow, unsigned nColumn);
-	void CursorRight (void);
-	void CursorUp (void);
-	void DisplayChar (char chChar);
-	void EraseChars (unsigned nCount);
-	void NewLine (void);
-	void SetAutoPageMode (boolean bEnable);
-	void SetCursorMode (boolean bVisible);
-	void Tabulator (void);
-
-	void Scroll (void);
-
-	void SetChar (unsigned nPosX, unsigned nPosY, char chChar);
-	void SetCursor (void);
+	void DevClearCursor (void) override;
+	void DevSetCursor (unsigned nCursorX, unsigned nCursorY) override;
+	void DevSetCursorMode (boolean bVisible) override;
+	void DevSetChar (unsigned nPosX, unsigned nPosY, char chChar) override;
+	void DevUpdateDisplay (void) override;
 
 	void WriteByte (u8 nData, int mode);
 	void WriteHalfByte (u8 nData, int mode);
@@ -136,9 +81,6 @@ private:
 	u8 GetDDRAMAddress (unsigned nPosX, unsigned nPosY);
 
 private:
-	unsigned m_nColumns;
-	unsigned m_nRows;
-
 	CGPIOPin  m_D4;
 	CGPIOPin  m_D5;
 	CGPIOPin  m_D6;
@@ -151,18 +93,6 @@ private:
 	u8 m_nAddress;
 
 	boolean m_bBlockCursor;
-
-	unsigned m_nState;
-	unsigned m_nCursorX;
-	unsigned m_nCursorY;
-	boolean  m_bCursorOn;
-	unsigned m_nParam1;
-	unsigned m_nParam2;
-	boolean  m_bAutoPage;
-
-	char m_Buffer[HD44780_MAX_ROWS][HD44780_MAX_COLUMNS];
-
-	CSpinLock m_SpinLock;
 };
 
 #endif
