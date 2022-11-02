f64e27fc0e5429315111ecc15a08236d19cb921f
Rene Stange <rsta2@o2online.de>
2022-11-02 22:59:41 +0100

    test: Add sound-controller
    * For testing sound controllers of sound devices.
    * Does only support sound output at the moment.

---
+++ test/sound-controller/kernel.h
@@ -0,0 +1,82 @@
+//
+// kernel.h
+//
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2014-2022  R. Stange <rsta2@o2online.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#ifndef _kernel_h
+#define _kernel_h
+
+#include <circle/actled.h>
+#include <circle/koptions.h>
+#include <circle/devicenameservice.h>
+#include <circle/exceptionhandler.h>
+#include <circle/interrupt.h>
+#include <circle/screen.h>
+#include <circle/serial.h>
+#include <circle/timer.h>
+#include <circle/logger.h>
+#include <circle/i2cmaster.h>
+#include <circle/usb/usbhcidevice.h>
+#include <circle/input/console.h>
+#include <circle/types.h>
+#include "soundshell.h"
+
+#ifdef USE_VCHIQ_SOUND
+	#include <circle/sched/scheduler.h>
+	#include <vc4/vchiq/vchiqdevice.h>
+#endif
+
+enum TShutdownMode
+{
+	ShutdownNone,
+	ShutdownHalt,
+	ShutdownReboot
+};
+
+class CKernel
+{
+public:
+	CKernel (void);
+	~CKernel (void);
+
+	boolean Initialize (void);
+
+	TShutdownMode Run (void);
+
+private:
+	// do not change this order
+	CActLED			m_ActLED;
+	CKernelOptions		m_Options;
+	CDeviceNameService	m_DeviceNameService;
+	CExceptionHandler	m_ExceptionHandler;
+	CInterruptSystem	m_Interrupt;
+	CScreenDevice		m_Screen;
+	CSerialDevice		m_Serial;
+	CTimer			m_Timer;
+	CLogger			m_Logger;
+	CI2CMaster		m_I2CMaster;
+	CUSBHCIDevice		m_USBHCI;
+#ifdef USE_VCHIQ_SOUND
+	CScheduler		m_Scheduler;
+	CVCHIQDevice		m_VCHIQ;
+#endif
+
+	CConsole		m_Console;
+	CSoundShell		m_SoundShell;
+};
+
+#endif
