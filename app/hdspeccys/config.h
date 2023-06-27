d3f3ad87b6b912163a12d7738dd1798588e1b239
Richard Sewell <10079653+zx-ncoderz@users.noreply.github.com>
2023-06-27 22:31:19 +1200

    wip: integrated OpenGL example into project

---
+++ app/hdspeccys/config.h
@@ -0,0 +1,22 @@
+//
+// config.h
+//
+// #ifndef _config_h
+// #define _config_h
+
+// TODO - move all this configuration out into the makefile(s)
+
+// #define HD_SPECCYS_SERIAL_BAUD_RATE		921600
+// #define HD_SPECCYS_SERIAL_BAUD_RATE		576000
+#define HD_SPECCYS_SERIAL_BAUD_RATE		460800
+// #define HD_SPECCYS_SERIAL_BAUD_RATE		230400
+// #define HD_SPECCYS_SERIAL_BAUD_RATE		115200
+// #define HD_SPECCYS_FEATURE_NETWORK 		TRUE
+#define HD_SPECCYS_FEATURE_NETWORK 		FALSE
+#define HD_SPECCYS_DHCP			 		TRUE
+
+#define HD_SPECCYS_FEATURE_OPENGL		TRUE
+// #define HD_SPECCYS_FEATURE_OPENGL		FALSE
+
+
+// #endif // _config_h
