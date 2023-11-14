96c9029b695d9c1f54a2ec0690bdedf6c9f122e4
Rene Stange <rsta2@o2online.de>
2023-11-14 20:41:50 +0100

    sample/01-gpiosimple: Remove audio for RPi 5

--- sample/01-gpiosimple/kernel.cpp
+++ sample/01-gpiosimple/kernel.cpp
@@ -2,7 +2,7 @@
 // kernel.cpp
 //
 // Circle - A C++ bare metal environment for Raspberry Pi
-// Copyright (C) 2016  R. Stange <rsta2@o2online.de>
+// Copyright (C) 2016-2023  R. Stange <rsta2@o2online.de>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,7 +18,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 #include "kernel.h"
-#include <circle/gpiopin.h>
+#if RASPPI <= 4
+	#include <circle/gpiopin.h>
+#endif
 #include <circle/timer.h>
 
 CKernel::CKernel (void)
@@ -36,15 +38,19 @@ boolean CKernel::Initialize (void)
 
 TShutdownMode CKernel::Run (void)
 {
+#if RASPPI <= 4
 	CGPIOPin AudioLeft (GPIOPinAudioLeft, GPIOModeOutput);
 	CGPIOPin AudioRight (GPIOPinAudioRight, GPIOModeOutput);
-	
+#endif
+
 	// flash the Act LED 10 times and click on audio (3.5mm headphone jack)
 	for (unsigned i = 1; i <= 10; i++)
 	{
 		m_ActLED.On ();
+#if RASPPI <= 4
 		AudioLeft.Invert ();
 		AudioRight.Invert ();
+#endif
 		CTimer::SimpleMsDelay (200);
 
 		m_ActLED.Off ();
