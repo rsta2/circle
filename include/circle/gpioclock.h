4e067efbc1aaec31a6929f5e7955f72be8b60629
Rene Stange <rsta2@o2online.de>
2024-03-25 19:40:41 +0100

    gpioclock: Start() returns status

--- include/circle/gpioclock.h
+++ include/circle/gpioclock.h
@@ -1,6 +1,22 @@
 //
 // gpioclock.h
 //
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2014-2024  R. Stange <rsta2@o2online.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef _circle_gpioclock_h
 #define _circle_gpioclock_h
 
@@ -37,9 +53,9 @@ public:
 	~CGPIOClock (void);
 
 						// refer to "BCM2835 ARM Peripherals" for that values:
-	void Start (unsigned	nDivI,		// 1..4095, allowed minimum depends on MASH
-		    unsigned	nDivF = 0,	// 0..4095
-		    unsigned	nMASH = 0);	// 0..3
+	boolean Start (unsigned	nDivI,		// 1..4095, allowed minimum depends on MASH
+		       unsigned	nDivF = 0,	// 0..4095
+		       unsigned	nMASH = 0);	// 0..3
 
 	// assigns clock source automatically
 	// returns FALSE if requested rate cannot be generated
