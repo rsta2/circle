50e4eaf8ad00133787c0dc94cf966b57a584447e
probonopd <probonopd@users.noreply.github.com>
2025-02-02 09:04:32 +0100

    Add support for SSD1305
    Add support for SSD1305 by @Sterr1.
    
    For details please see https://github.com/probonopd/MiniDexed/issues/808#issuecomment-2629132331

--- addon/display/ssd1306device.cpp
+++ addon/display/ssd1306device.cpp
@@ -134,11 +134,12 @@ boolean CSSD1306Device::Initialize (void)
 	assert(m_pI2CMaster != nullptr);
 
 	// Validate dimensions - only 128x32 and 128x64 supported for now
-	if (!(m_nHeight == 32 || m_nHeight == 64) || m_nWidth != 128)
+	if (!(m_nHeight == 32 || m_nHeight == 64) || !(m_nWidth == 128 || m_nWidth == 132))
 		return false;
 
+	const bool bIsSSD1305 = m_nWidth == 132;
 	const u8 nMultiplexRatio  = m_nHeight - 1;
-	const u8 nCOMPins         = m_nHeight == 32 ? 0x02 : 0x12;
+	const u8 nCOMPins         = (m_nHeight == 32 && !bIsSSD1305) ? 0x02 : 0x12;
 	const u8 nColumnAddrRange = m_nWidth - 1;
 	const u8 nPageAddrRange   = m_nHeight / 8 - 1;
 	// https://www.buydisplay.com/download/ic/SSD1312_Datasheet.pdf Pg. 51 Section 2.1.19
