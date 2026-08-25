da3bed7b1b89fd9ecbfd9777b6e821b8d1e8410f
Rene Stange <rsta2@gmx.net>
2026-08-25 12:58:19 +0200

    usb/usbmidihost: Add USB_MIDI_BOOST_EARLY_RESUBMIT (usbboost=4)
    A USB MIDI receive request will be resubmitted, before the previously
    received MIDI events have been processed, when this option is enabled.
    A second RX packet buffer is used for this purpose.
    
    Authored by @marchingband

--- include/circle/koptions.h
+++ include/circle/koptions.h
@@ -44,7 +44,8 @@ public:
 #define USB_MIDI_BOOST_NONE			0	// default
 #define USB_MIDI_BOOST_NO_DELAY_ON_IDLE		1	// do not delay to relax bus (same as "true")
 #define USB_MIDI_BOOST_NO_COMPLETE_ON_NAK	2	// do not complete on NAK
-#define USB_MIDI_BOOST_MAX			3
+#define USB_MIDI_BOOST_EARLY_RESUBMIT		4	// restart receive before event processing
+#define USB_MIDI_BOOST_MAX			7
 	const char *GetUSBIgnore (void) const;		// defaults to empty string
 
 	const unsigned *GetUSBSoundChannels (void) const; // returns 2 values
