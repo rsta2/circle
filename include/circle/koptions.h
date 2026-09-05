095cd2365689b2b69977fc459f9c7a7d10d9461d
Rene Stange <rsta2@gmx.net>
2026-09-05 13:56:54 +0200

    koptions: Swap usbboost= values 2 and 4
    Because now the values 1 and 2 are less serious and influential
    than 4 and 8.

--- include/circle/koptions.h
+++ include/circle/koptions.h
@@ -43,8 +43,8 @@ public:
 	unsigned GetUSBBoost (void) const;		// bit mask of the following values:
 #define USB_MIDI_BOOST_NONE			0	// default
 #define USB_MIDI_BOOST_NO_DELAY_ON_IDLE		1	// do not delay to relax bus (same as "true")
-#define USB_MIDI_BOOST_NO_COMPLETE_ON_NAK	2	// do not complete on NAK
-#define USB_MIDI_BOOST_EARLY_RESUBMIT		4	// restart receive before event processing
+#define USB_MIDI_BOOST_EARLY_RESUBMIT		2	// restart receive before event processing
+#define USB_MIDI_BOOST_NO_COMPLETE_ON_NAK	4	// do not complete on NAK
 #define USB_MIDI_BOOST_NOSPLIT_BULK_IMMEDIATE	8	// do not queue Bulk URBs, start immediately
 #define USB_MIDI_BOOST_MAX			15
 	const char *GetUSBIgnore (void) const;		// defaults to empty string
